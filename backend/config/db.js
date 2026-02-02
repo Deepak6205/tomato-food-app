587f0ed4e6e53cd8db670c9b4a723dbdd0e9e0d6
deepak kumar <deepak7292832956@gmail.com>
2026-02-02 08:17:07 +0530

    updating secrets

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-  await mongoose.connect("mongodb+srv://deepaktech:Dee6205@cluster0.zonzccq.mongodb.net/food-app").then(()=>console.log("DB Connected"))
+  await mongoose.connect(process.env.MONGO_URI).then(()=>console.log("DB Connected"))
 };
